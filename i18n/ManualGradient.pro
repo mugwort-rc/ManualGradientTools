c2a034be861012999a85999305171d00a9c43c8e
mugwort_rc <mugwort.rc@gmail.com>
2016-06-28 13:08:07 +0900

    Add translations.

---
+++ i18n/ManualGradient.pro
@@ -0,0 +1,8 @@
+SOURCES = \
+	../__init__.py \
+	../manual_gradient.py \
+	../manual_gradient_dialog.py
+
+FORMS = ../manual_gradient_dialog_base.ui
+
+TRANSLATIONS = ManualGradient_ja.ts
---
+++ i18n/ManualGradient_ja.ts
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>ManualGradient</name>
+    <message>
+        <location filename="../manual_gradient.py" line="189"/>
+        <source>&amp;Manual Gradient Tools</source>
+        <translation>手動分類ツール (&amp;M)</translation>
+    </message>
+    <message>
+        <location filename="../manual_gradient.py" line="179"/>
+        <source>Manual Gradient Tools</source>
+        <translation>手動分類ツール</translation>
+    </message>
+</context>
+<context>
+    <name>ManualGradientDialogBase</name>
+    <message>
+        <location filename="../manual_gradient_dialog_base.ui" line="14"/>
+        <source>Manual Gradient Tools</source>
+        <translation>手動分類ツール</translation>
+    </message>
+    <message>
+        <location filename="../manual_gradient_dialog_base.ui" line="20"/>
+        <source>Layer</source>
+        <translation>レイヤー</translation>
+    </message>
+    <message>
+        <location filename="../manual_gradient_dialog_base.ui" line="33"/>
+        <source>Field</source>
+        <translation>カラム</translation>
+    </message>
+    <message>
+        <location filename="../manual_gradient_dialog_base.ui" line="46"/>
+        <source>AltName</source>
+        <translation>レイヤー別名</translation>
+    </message>
+    <message>
+        <location filename="../manual_gradient_dialog_base.ui" line="60"/>
+        <source>Start</source>
+        <translation>開始値</translation>
+    </message>
+    <message>
+        <location filename="../manual_gradient_dialog_base.ui" line="83"/>
+        <source>Step</source>
+        <translation>間隔値</translation>
+    </message>
+    <message>
+        <location filename="../manual_gradient_dialog_base.ui" line="106"/>
+        <source>Count</source>
+        <translation>分類数</translation>
+    </message>
+    <message>
+        <location filename="../manual_gradient_dialog_base.ui" line="126"/>
+        <source>Suffix</source>
+        <translation>接尾辞</translation>
+    </message>
+</context>
+</TS>
